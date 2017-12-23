ec3986b88ec2e452f835cfe4557d76d54cca09e4
ioanabirsan <ioana.c.birsan@gmail.com>
2017-12-23 18:05:45 +0200

    Add initial completion support

---
+++ MyPlugin/src/com/kplugin/KCompletionContributor.java
@@ -0,0 +1,42 @@
+package com.kplugin;
+
+import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.patterns.PlatformPatterns;
+import com.intellij.util.ProcessingContext;
+import com.kplugin.psi.KTypes;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by AMI on 2017-12-23.
+ */
+
+
+public class KCompletionContributor extends CompletionContributor {
+    private static final List <String> suggestions =
+            Arrays.asList(
+                    "module",
+                    "endmodule",
+                    "syntax",
+                    "pgm", "imports", "configuration");
+
+    public KCompletionContributor() {
+        extend(CompletionType.BASIC,
+                PlatformPatterns.psiElement(KTypes.VALUE).withLanguage(KLanguage.INSTANCE),
+                new CompletionProvider <CompletionParameters>() {
+                    public void addCompletions(@NotNull CompletionParameters parameters,
+                                               ProcessingContext context,
+                                               @NotNull CompletionResultSet resultSet) {
+                        for (String suggestion : suggestions) {
+                            resultSet.addElement(LookupElementBuilder.create(suggestion));
+                        }
+                    }
+                }
+        );
+    }
+}
+
+
