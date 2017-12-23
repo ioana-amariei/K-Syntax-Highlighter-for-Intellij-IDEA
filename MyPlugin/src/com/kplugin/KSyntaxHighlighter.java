ee3f16983e8f0d64897775f9150e4f89c9d02db4
ioanabirsan <ioana.c.birsan@gmail.com>
2017-12-23 17:03:14 +0200

    Define and register a K syntax highlighter class

---
+++ MyPlugin/src/com/kplugin/KSyntaxHighlighter.java
@@ -0,0 +1,62 @@
+package com.kplugin;
+
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.HighlighterColors;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
+import com.intellij.psi.TokenType;
+import com.intellij.psi.tree.IElementType;
+import com.kplugin.psi.KTypes;
+import org.jetbrains.annotations.NotNull;
+
+import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
+
+/**
+ * Created by AMI on 2017-12-23.
+ */
+
+
+public class KSyntaxHighlighter extends SyntaxHighlighterBase {
+    public static final TextAttributesKey SEPARATOR =
+            createTextAttributesKey("K_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN);
+    public static final TextAttributesKey KEY =
+            createTextAttributesKey("K_KEY", DefaultLanguageHighlighterColors.KEYWORD);
+    public static final TextAttributesKey VALUE =
+            createTextAttributesKey("K_VALUE", DefaultLanguageHighlighterColors.STRING);
+    public static final TextAttributesKey COMMENT =
+            createTextAttributesKey("K_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
+    public static final TextAttributesKey BAD_CHARACTER =
+            createTextAttributesKey("K_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
+
+    private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
+    private static final TextAttributesKey[] SEPARATOR_KEYS = new TextAttributesKey[]{SEPARATOR};
+    private static final TextAttributesKey[] KEY_KEYS = new TextAttributesKey[]{KEY};
+    private static final TextAttributesKey[] VALUE_KEYS = new TextAttributesKey[]{VALUE};
+    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
+    private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
+
+    @NotNull
+    @Override
+    public Lexer getHighlightingLexer() {
+        return new KLexerAdapter();
+    }
+
+    @NotNull
+    @Override
+    public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
+        if (tokenType.equals(KTypes.SEPARATOR)) {
+            return SEPARATOR_KEYS;
+        } else if (tokenType.equals(KTypes.KEY)) {
+            return KEY_KEYS;
+        } else if (tokenType.equals(KTypes.VALUE)) {
+            return VALUE_KEYS;
+        } else if (tokenType.equals(KTypes.COMMENT)) {
+            return COMMENT_KEYS;
+        } else if (tokenType.equals(TokenType.BAD_CHARACTER)) {
+            return BAD_CHAR_KEYS;
+        } else {
+            return EMPTY_KEYS;
+        }
+    }
+}
---
+++ MyPlugin/src/com/kplugin/KSyntaxHighlighterFactory.java
@@ -0,0 +1,19 @@
+package com.kplugin;
+
+import com.intellij.openapi.fileTypes.*;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by AMI on 2017-12-23.
+ */
+
+
+public class KSyntaxHighlighterFactory extends SyntaxHighlighterFactory {
+    @NotNull
+    @Override
+    public SyntaxHighlighter getSyntaxHighlighter(Project project, VirtualFile virtualFile) {
+        return new KSyntaxHighlighter();
+    }
+}
